ba5a259eb147922a3c2a68894d63951bf976164e
Hải Long <hlonn.it@gmail.com>
2025-05-09 12:13:58 +0700

    Update environment.ts

--- EAM_CLI/src/environments/environment.ts
+++ EAM_CLI/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-   apiUrl: 'https://localhost:4008/api',
-   urlFiles: 'https://localhost:4008',
+  //  apiUrl: 'https://localhost:4008/api',
+  //  urlFiles: 'https://localhost:4008',
 
 
-  //  apiUrl: 'http://sso.d2s.com.vn:1081/api',
-  //  urlFiles: 'http://sso.d2s.com.vn:1081',
+   apiUrl: 'http://sso.d2s.com.vn:1081/api',
+   urlFiles: 'http://sso.d2s.com.vn:1081',
 };
